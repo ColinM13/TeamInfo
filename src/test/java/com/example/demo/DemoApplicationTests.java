3ac4175a038b88f228eb4e493447673a2fad643f
ColinM13 <colinmiller813@gmail.com>
2020-10-08 17:46:37 -0500

    Added initial testing

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+import com.example.demo.controller.TeamController;
+import com.example.demo.model.Team;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DemoApplicationTests extends AbstractTestNGSpringContextTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+
+	}
+
+	@Test
+	public void getTeamDallasStarsShouldReturnDallasStars() {
+		Assert.assertEquals(this.testRestTemplate.getForObject("http://localhost:" + port + "/teams/stars", Team.class).getName(), "Stars");
 	}
 
 }
